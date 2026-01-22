3a285862d666db38795d025dacdb2b514ff4a946
dhruti0 <dhruti2001@gmail.com>
2026-01-22 16:43:07 +0530

    my second commit

---
+++ demo_sts_javaproject/src/demo_sts_java_project/demo.java
@@ -0,0 +1,13 @@
+package demo_sts_java_project;
+
+public class demo {
+
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("sts");
+	}
+
+}
